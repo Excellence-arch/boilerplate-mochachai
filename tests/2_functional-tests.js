1c3b71f4fce4cac22cf7db76f0669ef8c06aada5
excellence-arch <oladipupomichael9@gmail.com>
2023-03-05 15:09:42 +0100

    passing all functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -99,7 +99,7 @@ suite("Functional Tests with Zombie.js", function () {
         browser.fill("surname", "Vespucci").then(() => {
           browser.pressButton("submit", () => {
             browser.assert.success();
-            browser.assert.text("span#name");
+            browser.assert.text("span#name", "Amerigo");
             browser.assert.text("span#surname", "Vespucci");
             browser.assert.elements("span#dates", 1);
             done();
